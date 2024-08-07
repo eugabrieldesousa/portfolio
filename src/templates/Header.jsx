387cacbc1f92ffb8df10dee3cf092a6d06f12655
“eugabrieldesousa” <eugabrieldesousa@gmail.com>
2024-08-07 15:41:24 -0300

    initial commit

---
+++ src/templates/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyComponent = () => {
+  return (
+    <div className="bg-[#212426] px-[10%] py-5 flex items-center justify-center">
+      <h1 className="text-white font-[700] tracking-wider">keep going</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
